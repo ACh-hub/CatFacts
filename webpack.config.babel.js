90ef56409e07dcfada56655f9b2ebe3cb0076b20
Aleksandra Chrapkowska <amchrapkowska@gmail.com>
2019-06-25 12:30:41 +0200

    initial commit

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -4,53 +4,22 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const I18nPlugin = require('i18n-webpack-plugin');
-
-// Will be moved to separate json file in the next iteration
-const languages = {
-	en: require('./languages/en.json'),
-	pl: require('./languages/pl.json')
-};
-
-
-const config = Object.keys(languages).map(lang => ({
-	name: lang,
-	entry: {
-		app: __dirname + '/src/js/app.js'
-	},
+module.exports = {
+	entry: './src/js/app.js',
 	mode: 'development',
-	
 	output: {
-		path: path.resolve(__dirname, './dist/' + lang),
-		path: __dirname + '/dist/' + lang,
-		publicPath: '/dist/' + lang,
-		filename: '[name]-[chunkhash].js'
-},
+		path: path.resolve(__dirname, './dist'),
+		filename: "app.js"
+	},
 	target: 'node',
 	module: {
-		rules: [
-
-		// Babel	
-			{
+		rules: [{
 			test: /\.js$/,
 			exclude: /node_modules/,
 			use: {
 				loader: 'babel-loader',
 			}
 		},
-		// Eslint
-		{
-			test: /\.js$/,
-			exclude: /node_modules/,
-			use: {
-				loader: 'eslint-loader',
-				options: {
-					fix: true,
-				  }, 
-			},
-		},
-		// Css
 		{
 			test: /\.scss$/,
 			use: [ 'style-loader', 
@@ -68,17 +37,8 @@ const config = Object.keys(languages).map(lang => ({
 	},
 	plugins: [
 		new CleanWebpackPlugin(),
-		new HtmlWebPackPlugin({
-			template:  __dirname + '/src/index.html'
-	}),
-	new MiniCssExtractPlugin({
-		filename: '[name]-[contenthash].css',
-		chunkFilename: '[id].css'
-}),
-new I18nPlugin(languages[lang])
-		]
-
-}));
-
-module.exports = config;
-
+		new MiniCssExtractPlugin({
+		  filename: 'styles.css',
+		}),
+	  ]
+};
