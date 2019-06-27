8c9326295b6f3a05131ca4db1dd065c5b28c4366
Aleksandra Chrapkowska <amchrapkowska@gmail.com>
2019-06-27 11:33:18 +0200

    testing

--- src/js/app.js
+++ src/js/app.js
@@ -6,23 +6,23 @@ import '../sass/styles.scss'
 import View from './modules/view';
 import Model from './modules/model';
 import Controller from './modules/controller';
-import { onLoad } from './modules/onload';
+import {
+  onLoad
+} from './modules/onload';
 
 
 class App {
-    constructor() {
-      this.model = new Model();
-      this.view = new View();
-      this.controller = new Controller(this.model,this.view);
-    };
-    init() {
-      onLoad(this.view.button,'click',()=>this.controller.updateFact())
-      this.view.renderAnimation();
-      this.controller.updateFact();
-    };
-  }
-  const app = new App();
-  
-  onLoad(window,'load',()=>app.init())
+  constructor() {
+    this.model = new Model();
+    this.view = new View();
+    this.controller = new Controller(this.model, this.view);
+  };
+  init() {
+    onLoad(this.view.button, 'click', () => this.controller.updateFact())
+    this.view.renderAnimation();
+    this.controller.updateFact();
+  };
+}
+const app = new App();
 
-
+onLoad(window, 'load', () => app.init())
